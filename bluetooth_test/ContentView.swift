d6796de69c779fe59dd150ca7091b9e0349e05e7
scott susanto <susantoscott@gmail.com>
2024-03-28 14:59:38 -0700

    Initial Commit

---
+++ bluetooth_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bluetooth_test
+//
+//  Created by Scott Susanto on 3/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bluetooth_test/bluetooth_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetooth_testApp.swift
+//  bluetooth_test
+//
+//  Created by Scott Susanto on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct bluetooth_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
